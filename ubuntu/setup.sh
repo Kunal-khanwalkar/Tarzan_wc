1b11f468f06c2fd416cac0b48771035eb9ef7636
kunal-khanwalkar <kunalkhanwalkar@gmail.com>
2019-03-31 11:46:36 +0530

    minor updates in installation script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -17,17 +17,19 @@ fi
 echo "*****Update Ubuntu*****"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install build-essential
-sudo apt-get install git
-sudo apt-get install pip 
-pip install --user wheel numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+sudo apt-get install build-essential -y
+sudo apt-get install git -y
+sudo apt-get install pip -y
+pip install --user wheel numpy scipy matplotlib scikit-image scikit-learn ipython dlib -y
 
 echo "*********ROS installation*********"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get install ros-"$dist"-desktop-full -y
 
 echo "*some dependencies*"
-sudo apt-get install v4l2ucp v4l-utils libv4l-dev
+sudo apt-get install v4l2ucp v4l-utils libv4l-dev -y
 
 echo "*********OpenCV installation*********"
-./opencv"$vr".sh
+sudo ./opencv"$vr".sh
 
